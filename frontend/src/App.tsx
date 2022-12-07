4ec23b34f63fa8df0477464b3fa75770f54d579d
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2022-12-07 16:16:36 +0545

    tailwind setup complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Button } from 'flowbite-react';
+import Style from './style.module.css';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className={Style.main}>
+			App
+			<Button color='failure'>Failure</Button>
+		</div>
+	);
 };
 
 export default App;
